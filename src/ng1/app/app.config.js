0f9f14d0cf2e2529c193ff5aa04d149a1aba6368
LPathirana <lahiru.pathirana@cambio.lk>
2020-04-08 15:24:51 +0530

    use the downgraded component inside ng1 routes

--- src/ng1/app/app.config.js
+++ src/ng1/app/app.config.js
@@ -5,12 +5,15 @@ angular.
   config(['$routeProvider',
     function config($routeProvider) {
       $routeProvider.
-        when("/", {
+        when("/", { // AngularJS 1.x
           templateUrl: "ng1/app/html/home.html",
           controller: "SearchController"
         }).
         when("/test", {
-          template: '<h1>lahiru</h1>'
+          template: '<h1>lahiru</h1>' // AngularJS 1.x
+        }).
+        when('/ng2', {
+          template: '<ng2-dg></ng2-dg>' // downgraded Angular component
         }).
         otherwise("/404");
     }
